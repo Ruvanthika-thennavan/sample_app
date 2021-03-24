e0df5456e7bb8c6d0408bdfd55c4977fb50b9885
Ruvanthika-thennavan <ruvanthika.thennavan@freshworks.com>
2021-03-24 11:47:36 +0530

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 	def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
 
   end
   def new
@@ -49,13 +50,7 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
     # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in to edit ."
-        redirect_to login_url
-      end
-    end
+   
     # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
